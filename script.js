065c9be9ec0f5834a4ee2d8a29ced4646966a060
sinned8 <dennisdust2@gmail.com>
2023-10-16 15:56:45 -0400

    Added book cards HTML and CSS. Started working on JS constructors for the library and book cards.

--- script.js
+++ script.js
@@ -7,3 +7,62 @@ closePopUp.addEventListener('click',() => popUp.style.display = 'none')
 
 const newBookBtn = document.querySelector('.newbtn');
 newBookBtn.addEventListener('click', () => popUp.style.display = 'block')
+
+
+// Constructor for books
+class Book{
+    constructor(
+        title = 'Unkown',
+        author = 'Unkown',
+        pages = 0,
+        isRead = false
+    ) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.isRead = isRead
+    }
+}
+
+// Constructor for Library
+class Library{
+    constructor() {
+        this.books = []
+    }
+    addBook(newBook){
+        this.books.push(newBook)
+    }
+    removeBook(title) {
+        this.books = this.books.filter((book) => book.title !== title)
+    }
+    getTitle(title) {
+        return this.books.find(book => book.title === title)
+    }
+
+    isInLibrary(title) {
+        return this.books.some(book => book.title === title)
+    }
+}
+const library = new Library()
+
+// Handling and creating book from form input data
+const bookFromInput = () => {
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const isRead = document.getElementById('is-read').checked
+
+    return new Book(title, author, pages, isRead)
+
+}
+
+const addNewBook = (e) => {
+    e.preventDefault()
+    const newBook = bookFromInput()
+    if(library.isInLibrary(newBook.title)) {
+        alert('Book already exists')
+    } else {
+        library.addBook(newBook)
+        
+    }
+}
